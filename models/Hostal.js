bba5d8d12537baf1d4f714efffd53df87e9e80bf
Alejandro Garcia Flores <a.garcia13@uniandes.edu.co>
2019-03-03 19:47:04 -0500

    Pruebas Hostales

--- models/Hostal.js
+++ models/Hostal.js
@@ -2,8 +2,8 @@ import uuid from 'uuid';
 
 class Hostal {
     constructor(){
-        this.hostales =[];
-    }
+        this.hostals =[];
+    };
 
     create(data){
         if(this.findOne(data.nombre)){
@@ -11,51 +11,51 @@ class Hostal {
         }
         const newHostal = {
             idHostal: uuid.v4(),
-            nombre: data.nombre || ' ',
-            descripcion: data.descripcion || ' ',
-            precio: data.precio || ' ',
-            telefono: data.telefono || ' ',
-            sitioWeb: data.sitioWeb || ' ',
-            ciudad: data.ciudad || ' ',
-            direccion: data.direccion || ' ',
-            puntuacion: data.puntuacion || ' '
+            nombre: data.nombre || '',
+            descripcion: data.descripcion || '',
+            precio: data.precio || '',
+            telefono: data.telefono || '',
+            sitioWeb: data.sitioWeb || '',
+            ciudad: data.ciudad || '',
+            direccion: data.direccion || '',
+            puntuacion: data.puntuacion || ''
         };
-        this.hostales.push(newHostal);
+        this.hostals.push(newHostal);
         return newHostal;
-    }
+    };
 
     findOne(nombre){
-        return this.hostales.find(hostal => hostal.nombre === nombre);
-    }
+        return this.hostals.find(hostal => hostal.nombre === nombre);
+    };
 
     findAll(){
-        return this.hostales;
-    }
+        return this.hostals;
+    };
 
     update(nombre, data){
         const hostal = this.findOne(nombre);
-        const index = this.hostales.indexOf(hostal);
+        const index = this.hostals.indexOf(hostal);
 
-        this.hostales[index].nombre = data['nombre'] || hostal.nombre;
-        this.hostales[index].descripcion = data['descripcion'] || hostal.descripcion;
-        this.hostales[index].precio = data['precio'] || hostal.precio;
-        this.hostales[index].telefono = data['telefono'] || hostal.telefono;
-        this.hostales[index].sitioWeb = data['sitioWeb'] || hostal.sitioWeb;
-        this.hostales[index].ciudad = data['ciudad'] || hostal.ciudad;
-        this.hostales[index].direccion = data['direccion'] || hostal.direccion;
+        this.hostals[index].nombre = data['nombre'] || hostal.nombre;
+        this.hostals[index].descripcion = data['descripcion'] || hostal.descripcion;
+        this.hostals[index].precio = data['precio'] || hostal.precio;
+        this.hostals[index].telefono = data['telefono'] || hostal.telefono;
+        this.hostals[index].sitioWeb = data['sitioWeb'] || hostal.sitioWeb;
+        this.hostals[index].ciudad = data['ciudad'] || hostal.ciudad;
+        this.hostals[index].direccion = data['direccion'] || hostal.direccion;
         if(data['puntuacion']){
-            this.hostales[index].puntuacion = data['puntuacion'];
+            this.hostals[index].puntuacion = data['puntuacion'];
         }
         if(data['puntuacion'] === 0){
-            this.hostales[index].puntuacion = 0;
+            this.hostals[index].puntuacion = 0;
         }
-    }
+    };
 
     delete(nombre){
         const hostal = this.findOne(nombre);
-        const index = this.hostales.indexOf(hostal);
-        this.hostales.splice(index,1);
+        const index = this.hostals.indexOf(hostal);
+        this.hostals.splice(index,1);
         return {};
-    }
+    };
 }
 export default Hostal;
--- src/controllers/Hostal.js
+++ src/controllers/Hostal.js
@@ -2,68 +2,68 @@ import HostalModel from '../../models/Hostal';
 import fs from 'fs';
 import util from 'util';
 
-const Hostal ={
-    
-    create(req, res){
-        const hostal = HostalModel.create(req.body);
-        fs.appendFileSync("./JSON/hostal.json", util.inspect(hostal) + "\r\n", function (err) {
-          if (err) {
-            return console.log(err);
-          }
-        });
-        return res.status(201).send(hostal);
-    },
+const Hostal = {
 
-    getAll(req, res) {
-        const hostal = HostalModel.findAll();
-        fs.appendFileSync("./JSON/hostal.json",util.inspect(hostal)+"\r\n",function(err){
-            if(err){
-                return console.log(err);
-            }
-        });
-        return res.status(200).send(hostal);
-      },
-      
-      getOne(req, res) {
-        const hostal= HostalModel.findOne(req.params.id);
-        if (!hostal){
-          return res.status(404).send({'message': 'hostal not found'});
-        }
-        fs.appendFileSync("./JSON/hostal.json",util.inspect(hostal)+"\r\n",function(err){
-            if(err){
-                return console.log(err);
-            }
-        });
-        return res.status(200).send(hostal);
-      },
-    
-      update(req, res) {
-        const hostal= HostalModel.findOne(req.params.id);
-        if (!hostal) {
-          return res.status(404).send({'message': 'hostal not found'});
-        }
-        const updatedHostal= HostalModel.update(req.params.id, req.body)
-        fs.appendFileSync("./JSON/hostal.json",util.inspect(updatedHostal)+"\r\n",function(err){
-            if(err){
-                return console.log(err);
-            }
-        });
-        return res.status(200).send(updatedHostal);
-      },
-    
-      delete(req, res) {
-        const hostal= HostalModel.findOne(req.params.id);
-        if (!hostal) {
-          return res.status(404).send({'message': 'hostal not found'});
-        }
-        fs.appendFileSync("./JSON/hostal.json",util.inspect(hostal)+"\r\n",function(err){
-            if(err){
-                return console.log(err);
-            }
-        });
-        const ref = HostalModel.delete(req.params.id);
-    
-        return res.status(204).send(ref);
+  create(req, res) {
+    const hostal = HostalModel.create(req.body);
+    fs.appendFileSync("./JSON/hostals.json", util.inspect(hostal) + "\r\n", function (err) {
+      if (err) {
+        return console.log(err);
       }
+    });
+    return res.status(201).send(hostal);
+  },
+
+  getAll(req, res) {
+    const hostals = HostalModel.findAll();
+    fs.appendFileSync("./JSON/hostals.json", util.inspect(hostals) + "\r\n", function (err) {
+      if (err) {
+        return console.log(err);
+      }
+    });
+    return res.status(200).send(hostals);
+  },
+
+  getOne(req, res) {
+    const hostal = HostalModel.findOne(req.params.id);
+    if (!hostal) {
+      return res.status(404).send({ 'message': 'hostal not found' });
+    }
+    fs.appendFileSync("./JSON/hostals.json", util.inspect(hostal) + "\r\n", function (err) {
+      if (err) {
+        return console.log(err);
+      }
+    });
+    return res.status(200).send(hostal);
+  },
+
+  update(req, res) {
+    const hostal = HostalModel.findOne(req.params.id);
+    if (!hostal) {
+      return res.status(404).send({ 'message': 'hostal not found' });
+    }
+    const updateHostal = HostalModel.update(req.params.id, req.body)
+    fs.appendFileSync("./JSON/hostals.json", util.inspect(updateHostal) + "\r\n", function (err) {
+      if (err) {
+        return console.log(err);
+      }
+    });
+    return res.status(200).send(updateHostal);
+  },
+
+  delete(req, res) {
+    const hostal = HostalModel.findOne(req.params.id);
+    if (!hostal) {
+      return res.status(404).send({ 'message': 'hostal not found' });
+    }
+    fs.appendFileSync("./JSON/hostals.json", util.inspect(hostal) + "\r\n", function (err) {
+      if (err) {
+        return console.log(err);
+      }
+    });
+    const ref = HostalModel.delete(req.params.id);
+    return res.status(204).send(ref);
+  }
 }
+
 export default Hostal;
